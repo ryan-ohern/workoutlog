3a82c8d631fa9945d3bcb3f0e64885bb4e1e1927
Ryan O'Hern <ryanohern@gmail.com>
2016-07-20 15:37:13 -0400

    added json web tokens

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,21 +1,25 @@
 var router = require('express').Router();
 var sequelize = require('../db.js');
 var User = sequelize.import('../models/user');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 router.post('/', function(req, res){
 
 	var username = req.body.user.username;
-	var pass = req.body.user.password; // TODO: hash this password
+	var pass = req.body.user.password;
 
 	// User model communicates with Postgres to create a new user
 	User.create({
 		username: username,
-		passwordhash: '' // TODO: make it hashed
+		passwordhash: bcrypt.hashSync(pass, 10)
 	}).then(
 		function createSuccess(user) {
+			var token = jwt.sign({ id: user.id }, "shh_I_am_secret", { expiresIn: 60*60*24 } );
 			res.json({
 				user: user,
-				message: 'created'
+				message: 'created',
+				sessionToken: token
 			});
 		},
 		function createError(err) {
@@ -23,7 +27,5 @@ router.post('/', function(req, res){
 		}
 	);
 });
-
-// router.route(/user)
 
 module.exports = router;

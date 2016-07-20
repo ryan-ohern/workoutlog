6d897e0e03bba480c8373feeeb6dc6943c5f88c7
Ryan O'Hern <ryanohern@gmail.com>
2016-07-20 14:56:34 -0400

    updated header file to allow GET, POST, PUT, & DELETE

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -1,6 +1,10 @@
-// allows api to be accessed by browsers or other elements outside of port 3000
 module.exports = function(req, res, next){
+	// allows api to be accessed by browsers or other elements outside of port 3000
 	res.header('access-control-allow-origin', '*');
+	// GET, POST, PUT & DELETE are allowed on this API
+	res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
+	res.header('access-control-allow-headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	next();
 };
 
